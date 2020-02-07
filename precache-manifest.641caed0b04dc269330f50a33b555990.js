d6cfbf2b3a91fef53a98bf186eb8096642ad160e
Tien Pham <tien.phamnguyen@powergatesoftware.com>
2020-02-07 10:37:05 +0700

    Updates

---
+++ precache-manifest.641caed0b04dc269330f50a33b555990.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18bdbb9ec654299ef31758b17e87c0ea",
+    "url": "/ghpageloto/index.html"
+  },
+  {
+    "revision": "aada3f968646f43099ad",
+    "url": "/ghpageloto/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "201af372ad8dcc046f8b",
+    "url": "/ghpageloto/static/js/2.0fbdbeb1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ghpageloto/static/js/2.0fbdbeb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aada3f968646f43099ad",
+    "url": "/ghpageloto/static/js/main.85d12f73.chunk.js"
+  },
+  {
+    "revision": "b80b7c03445b3abda240",
+    "url": "/ghpageloto/static/js/runtime-main.6e2acc45.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghpageloto/static/media/logo.5d5d9eef.svg"
+  }
+]);
